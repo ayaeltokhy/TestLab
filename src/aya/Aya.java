50f937d5356df6012621d37afb9787738da8d646
aya Eltokhy <aya_eltokhy@hotmail.com>
2017-02-26 10:28:41 +0200

    adding 2 var x , y

---
+++ src/aya/Aya.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aya;
+
+/**
+ *
+ * @author aya_e
+ */
+public class Aya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int x = 5;
+        int y = 50;
+    }
+    
+}
